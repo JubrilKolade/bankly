63477c5668e67693eb366933f3b5788c7dfea396
JubrilKolade <jubrilkolade97@gmail.com>
2025-12-17 03:20:44 +0100

    feat: pagination and progress bar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 import BankCard from './BankCard'
 import { countTransactionCategories } from '@/lib/utils'
 import Category from './Category'
